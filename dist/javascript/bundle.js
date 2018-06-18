620977f94db9a52865f05fe5afeb3030fd8aa989
Alfred Alejandrino <alejandrino.alfred@gmail.com>
2018-06-18 00:55:04 -0700

    include bundle.js so github hosts properly

---
+++ dist/javascript/bundle.js
@@ -0,0 +1,138 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = "./javascript/main.js");
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./javascript/main.js":
+/*!****************************!*\
+  !*** ./javascript/main.js ***!
+  \****************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+// Initialize canvas and display splash
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("Hey there and welcome to TetrisPuzzle");
+
+  // const canvas = document.getElementById('canvas');
+  const canvasEl = document.getElementById("canvas");
+  canvasEl.width = 750;
+  canvasEl.height = 700;
+
+  const ctx = canvasEl.getContext("2d");
+
+  ctx.fillStyle = "lightgrey";
+  for (let i = 150; i < 600; i=i+45) {
+    for (let j = 75; j < 525; j=j+45) {
+      ctx.fillRect(i, j, 45, 45);
+      ctx.rect(i, j, 45, 45);
+    }
+  }
+
+  ctx.fillStyle = "blue";
+  ctx.fillRect(100, 600, 45, 45);
+  ctx.fillRect(100, 555, 45, 45);
+  ctx.fillRect(145, 555, 45, 45);
+  ctx.fillRect(190, 555, 45, 45);
+  ctx.rect(100, 600, 45, 45);
+  ctx.rect(100, 555, 45, 45);
+  ctx.rect(145, 555, 45, 45);
+  ctx.rect(190, 555, 45, 45);
+
+
+  ctx.strokeStyle="#000000";
+  ctx.stroke();
+
+
+
+
+});
+
+
+/***/ })
+
+/******/ });
+//# sourceMappingURL=bundle.js.map
